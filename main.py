2ef4ec50c470f430fa939e38da828eb08a0e24f8
Maxim Kirillov <maximkir181210@gmail.com>
2023-03-18 00:17:16 +0300

    Создал класс Book

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+# Задание 2.
+# Реализуйте класс «Книга». Необходимо хранить в полях класса:
+# название книги, год выпуска, издателя, жанр, автора, цену. Реализуйте
+# конструктор по умолчанию и метод для вывода данных.
+class Book:
+    book_title: str
+    book_release: str
+    publisher: str
+    genre: str
+    author: str
+    price: str
+
 def execute_application():
     pass
 
