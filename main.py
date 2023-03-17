096352dcc6a884c131f81b92ebcfc43bc5158c4c
Maxim Kirillov <maximkir181210@gmail.com>
2023-03-17 22:39:59 +0300

    Создал класс Human

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+# Задание 1.
+# Реализуйте класс «Человек». Необходимо хранить в полях класса: ФИО,
+# дату рождения, контактный телефон, город, страну, домашний адрес.
+# Реализуйте конструктор по умолчанию и метод для вывода данных.
+class Human:
+    full_name: dict[str, str]
+    date_of_birth: dict[str, str]
+    phone: str
+    city: str
+    country: str
+    address: dict[str, str]
+
+
 def execute_application():
     pass
 
