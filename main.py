5859722e31147e4dd9967dfebbfdb4dfcfd6763e
Maxim Kirillov <maximkir181210@gmail.com>
2023-03-18 23:16:25 +0300

    Создал конструктор класса Stadium

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ class Stadium:
     city: str
     capacity: int
 
+    def __init__(self, stadium_name: str, opening_date: dict[str, int], country: str, city: str, capacity: int):
+        self.stadium_name = stadium_name
+        self.opening_date = opening_date
+        self.country = country
+        self.city = city
+        self.capacity = capacity
+
 
 def execute_application():
     pass
