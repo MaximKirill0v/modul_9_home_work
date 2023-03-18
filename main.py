f518b0bc516fadb6158a01f95b8d1cbab45191d2
Maxim Kirillov <maximkir181210@gmail.com>
2023-03-18 23:12:31 +0300

    Создал класс Stadium

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+# Задание 2.
+# Реализуйте класс «Стадион». Необходимо хранить в полях класса:
+# название стадиона, дату открытия, страну, город, вместимость. Реализуйте
+# конструктор по умолчанию и метод для вывода данных.
+
+class Stadium:
+    stadium_name: str
+    opening_date: dict[str, int]
+    country: str
+    city: str
+    capacity: float
+
+
 def execute_application():
     pass
 
