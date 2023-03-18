4a0be9d30750b3922464a614b8422966cee24aee
Maxim Kirillov <maximkir181210@gmail.com>
2023-03-18 22:33:00 +0300

    Создал класс Car

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+# Задание 1.
+# Реализуйте класс «Автомобиль». Необходимо хранить в полях класса:название модели,
+# год выпуска, производителя, объем двигателя, цвет машины, цену. Реализуйте конструктор по умолчанию
+# и метод для вывода данных.
+
+class Car:
+    name_model: str
+    year_of_manufacture: int
+    manufacturer: str
+    engine_capacity: float
+    color: str
+    price: float
+
+
 def execute_application():
     pass
 
