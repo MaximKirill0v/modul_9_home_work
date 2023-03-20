246132909c7c30a21f8995329368566403899229
Maxim Kirillov <maximkir181210@gmail.com>
2023-03-20 23:41:04 +0300

    Реализовал доступ к полям класса Human через геттеры и сеттеры

--- main.py
+++ main.py
@@ -25,6 +25,54 @@ class Human:
                f"Город: {self.__city}\n" \
                f"Адрес: {address}"
 
+    @property
+    def phone(self):
+        return self.__phone
+
+    @phone.setter
+    def phone(self, phone: str):
+        self.__phone = phone
+
+    @property
+    def city(self):
+        return self.__city
+
+    @city.setter
+    def city(self, city: str):
+        self.__city = city
+
+    @property
+    def country(self):
+        return self.__country
+
+    @country.setter
+    def country(self, country: str):
+        self.__country = country
+
+    @property
+    def full_name(self):
+        return self.__full_name
+
+    @full_name.setter
+    def full_name(self, full_name: dict[str, str]):
+        self.__full_name = full_name
+
+    @property
+    def date_of_birth(self):
+        return self.__date_of_birth
+
+    @date_of_birth.setter
+    def date_of_birth(self, date_of_birth: dict[str, str]):
+        self.__date_of_birth = date_of_birth
+
+    @property
+    def address(self):
+        return self.__address
+
+    @address.setter
+    def address(self, address: dict[str, str]):
+        self.__address = address
+
 
 def execute_application():
     full_name = {"Фамилия": "Иванов", "Имя": "Иван", "Отчество": "Иванович"}
@@ -32,6 +80,13 @@ def execute_application():
     address = {"Страна": "Россия", "Область": "Ярославская", "Город": "Ярославль", "Улица": "Пионерская", "Дом": "1",
                "Квартира": "1"}
     human_1 = Human(full_name, date_of_birth, "8(999)999-99-99", "Ярославль", "Россия", address)
+    print(human_1)
+    print(human_1.phone)
+    print(human_1.city)
+    print(human_1.country)
+    print(human_1.full_name)
+    print(human_1.date_of_birth)
+    print(human_1.address)
 
 
 if __name__ == '__main__':
