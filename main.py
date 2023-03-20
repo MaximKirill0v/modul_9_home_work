8bf17f29bd47dd9b8163e6919bd1da61e4f3f484
Maxim Kirillov <maximkir181210@gmail.com>
2023-03-20 23:53:34 +0300

    Создал объект класса Book

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Book:
 
 
 def execute_application():
-    pass
+    my_book = Book("Вниз по волшебной реке", 2019, "АСТ", "Приключения", "Успенский Э.Н.", 250)
 
 
 if __name__ == '__main__':
