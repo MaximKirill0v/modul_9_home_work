abd056b057850c60f960c58521f6b0ef35182f32
Maxim Kirillov <maximkir181210@gmail.com>
2023-03-20 23:52:16 +0300

    Проинициализировал поля класса Book с помощью приватного модификатора

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+# Задание 2.
+# Реализуйте класс «Книга». Необходимо хранить в полях класса:
+# название книги, год выпуска, издателя, жанр, автора, цену. Реализуйте
+# конструктор по умолчанию и метод для вывода данных. Реализуйте доступ к
+# отдельным полям класса через методы класса (геттеры и сеттеры).
+class Book:
+    def __init__(self, book_title: str, book_release: int, publisher: str, genre: str, author: str, price: float):
+        self.__book_title = book_title
+        self.__book_release = book_release
+        self.__publisher = publisher
+        self.__genre = genre
+        self.__author = author
+        self.__price = price
+
+
 def execute_application():
     pass
 
