74fc65cf21b0ddf343ecf05bae8f01da7cf49c2c
Maxim Kirillov <maximkir181210@gmail.com>
2023-03-21 23:18:58 +0300

    Внёс доработки в поля класса Human

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import Dict
+
+
 # Задание 1.
 # Реализуйте класс «Человек». Необходимо хранить в полях класса: ФИО,
 # дату рождения, контактный телефон, город, страну, домашний адрес.
@@ -5,25 +8,24 @@
 # Реализуйте доступ к отдельным полям класса через методы класса (геттеры
 # и сеттеры).
 class Human:
-    def __init__(self, full_name: dict[str, str], date_of_birth: dict[str, str], phone: str, city: str, country: str,
-                 address: dict[str, str]):
-        self.__full_name = full_name
-        self.__date_of_birth = date_of_birth
+    def __init__(self, full_name: Dict[str, str], date_of_birth: Dict[str, str], phone: str, city: str, country: str,
+                 address: Dict[str, str]):
+        self.__full_name = full_name.copy()
+        self.__date_of_birth = date_of_birth.copy()
         self.__phone = phone
         self.__city = city
         self.__country = country
-        self.__address = address
+        self.__address = address.copy()
 
     def __str__(self):
-        full_name = ", ".join(["-".join([key, value]) for key, value in self.__full_name.items()])
-        date_of_birth = ".".join([value for value in self.__date_of_birth.values()])
-        address = ", ".join(["-".join([key, value]) for key, value in self.__address.items()])
-        return f"ФИО: {full_name}\n" \
-               f"Дата рождения: {date_of_birth}\n" \
+        return f"ФИО: {' '.join(self.full_name.values())}\n" \
+               f"Дата рождения: {'.'.join(self.date_of_birth.values())}\n" \
                f"Контактный телефон: {self.__phone}\n" \
                f"Страна: {self.__country}\n" \
                f"Город: {self.__city}\n" \
-               f"Адрес: {address}"
+               f"Адрес: Страна: {self.address['Страна']}, Область: {self.address['Область']}," \
+               f" Город: {self.address['Город']}, Улица: {self.address['Улица']}, Дом: {self.address['Дом']}, " \
+               f"Квартира: {self.address['Квартира']}."
 
     @property
     def phone(self):
@@ -81,12 +83,13 @@ def execute_application():
                "Квартира": "1"}
     human_1 = Human(full_name, date_of_birth, "8(999)999-99-99", "Ярославль", "Россия", address)
     print(human_1)
-    print(human_1.phone)
-    print(human_1.city)
-    print(human_1.country)
-    print(human_1.full_name)
-    print(human_1.date_of_birth)
-    print(human_1.address)
+    print("\nОбращение к отдельным полям при помощи методов класса 'get': ")
+    print(f"Телефон экземпляра класса Human - {human_1.phone}")
+    print(f"Город экземпляра класса Human - {human_1.city}")
+    print(f"Страна экземпляра класса Human - {human_1.country}")
+    print(f"ФИО экземпляра класса Human - {human_1.full_name}")
+    print(f"Дата рождения экземпляра класса Human - {human_1.date_of_birth}")
+    print(f"Адрес экземпляра класса Human - {human_1.address}")
 
 
 if __name__ == '__main__':
