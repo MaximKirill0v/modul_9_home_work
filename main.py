9ea05478cd7427e226c89d194bdca4ac95de088f
Maxim Kirillov <maximkir181210@gmail.com>
2023-03-21 23:31:24 +0300

    Создал метод для вывода данных класса Car

--- main.py
+++ main.py
@@ -15,9 +15,18 @@ class Car:
         self.__color = color
         self.__price = price
 
+    def __str__(self):
+        return f"Модель автомобиля - '{self.__name_model}'.\n" \
+               f"Год выпуска - {self.__year_of_manufacture}г.\n" \
+               f"Производитель - {self.__manufacturer}.\n" \
+               f"Объём двигателя - {self.__engine_capacity}л.\n" \
+               f"Цвет кузова - {self.__color}.\n" \
+               f"Цена - {self.__price:,.2f}р."
+
 
 def execute_application():
     my_car = Car("Mustang", 2017, "Ford", 5.0, "Red", 3500000)
+    print(my_car)
 
 
 if __name__ == '__main__':
