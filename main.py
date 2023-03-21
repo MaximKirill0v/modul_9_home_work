8f1deb5e2c5cb496af643bf95e02191e809b3724
Maxim Kirillov <maximkir181210@gmail.com>
2023-03-21 23:40:19 +0300

    Реализовал доступ к приватным полям класса Car через методы get и set

--- main.py
+++ main.py
@@ -23,6 +23,54 @@ class Car:
                f"Цвет кузова - {self.__color}.\n" \
                f"Цена - {self.__price:,.2f}р."
 
+    @property
+    def name_model(self):
+        return self.__name_model
+
+    @name_model.setter
+    def name_model(self, name_model):
+        self.__name_model = name_model
+
+    @property
+    def year_of_manufacture(self):
+        return self.__year_of_manufacture
+
+    @year_of_manufacture.setter
+    def year_of_manufacture(self, year_of_manufacture):
+        self.__year_of_manufacture = year_of_manufacture
+
+    @property
+    def manufacturer(self):
+        return self.__manufacturer
+
+    @manufacturer.setter
+    def manufacturer(self, manufacturer):
+        self.__manufacturer = manufacturer
+
+    @property
+    def engine_capacity(self):
+        return self.__engine_capacity
+
+    @engine_capacity.setter
+    def engine_capacity(self, engine_capacity):
+        self.__engine_capacity = engine_capacity
+
+    @property
+    def color(self):
+        return self.__color
+
+    @color.setter
+    def color(self, color):
+        self.__color = color
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, price):
+        self.__price = price
+
 
 def execute_application():
     my_car = Car("Mustang", 2017, "Ford", 5.0, "Red", 3500000)
