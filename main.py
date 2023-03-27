0e376e86d49b79da94a337a9ecf483e9f087d007
Maxim Kirillov <maximkir181210@gmail.com>
2023-03-27 23:00:20 +0300

    Создал статический метод celsius_to_fahrenheit

--- main.py
+++ main.py
@@ -4,11 +4,26 @@
 # перевода из Цельсия в Фаренгейты и для перевода из Фаренгейта в Цельсия.
 
 class Temperature:
-    pass
+
+    @staticmethod
+    def celsius_to_fahrenheit(temperature: float):
+        return temperature * 1.8 + 32
 
 
 def execute_application():
-    pass
+    interface_data = input("Введите '1', если хотите перевести из градусов Цельсия в градусы по Фаренгейту,\n"
+                           "Введите '2', если хотите перевести из градусов по Фаренгейту в градусы Цельсия\n"
+                           "Введите '0', если хотите завершить программу.\n==> ")
+    while interface_data != '0':
+        if interface_data == '1':
+            temperature_celsius = input("Введите температуру в градусах по Цельсию: ")
+            if temperature_celsius.isdigit():
+                res_temperature = Temperature.celsius_to_fahrenheit(float(temperature_celsius))
+                print(f"Температура в переводе на градусы по Фаренгейту равна - {res_temperature}F.")
+            else:
+                print("Введённое значение не корректно. Повторите ввод.")
+                interface_data = input("Повторите ввод: ")
+
 
 
 if __name__ == '__main__':
