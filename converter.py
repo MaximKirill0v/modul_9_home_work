4d09e2ccdce35affc213441a0f1a356f7823ed76
Maxim Kirillov <maximkir181210@gmail.com>
2023-03-28 18:28:52 +0300

    Создал класс UnitConverter в файле UnitConverter.py

---
+++ converter.py
@@ -0,0 +1,12 @@
+class Converter:
+
+    @staticmethod
+    def millimeters_to_inches(millimeters: float):
+        return millimeters * 0.0394
+
+    @staticmethod
+    def inches_to_millimeters(inches: float):
+        return inches * 25.4
+
+
+
