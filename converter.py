26e205b089fcf2fa29261bf3f209b9fb8f761701
Maxim Kirillov <maximkir181210@gmail.com>
2023-03-29 23:21:06 +0300

    Реализовал статические методы конвертации единиц измерения

--- converter.py
+++ converter.py
@@ -1,4 +1,4 @@
-class Converter:
+class UnitConverter:
 
     @staticmethod
     def millimeters_to_inches(millimeters: float):
@@ -7,6 +7,22 @@ class Converter:
     @staticmethod
     def inches_to_millimeters(inches: float):
         return inches * 25.4
+
+    @staticmethod
+    def meters_to_foot(meters: float):
+        return meters * 3.281
+
+    @staticmethod
+    def foot_to_meters(foot: float):
+        return foot * 0.305
+
+    @staticmethod
+    def kilograms_to_lb(kilograms: float):
+        return kilograms * 2.205
+
+    @staticmethod
+    def lb_to_kilograms(lb: float):
+        return lb * 0.454
 
 
 
