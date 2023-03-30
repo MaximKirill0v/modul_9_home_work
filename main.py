0bc2774c7b51f5cc049a37fa543d377c13f8015c
Maxim Kirillov <maximkir181210@gmail.com>
2023-03-30 09:02:04 +0300

    Создал класс Device

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+from typing import Dict
+
+
+# Задание 1.
+# Создайте класс Device, который содержит информацию об устройстве.
+# С помощью механизма наследования, реализуйте класс CoffeeMachine
+# (содержит информацию о кофемашине), класс Blender (содержит информацию
+# о блендере).
+class Device:
+    def __init__(self, device_name: str, type_of_power: str, size: Dict[str, float], price: float):
+        self.__device_name = device_name
+        self.__type_of_power = type_of_power
+        self.__size = size.copy()
+        self.__price = price
+
+
 def execute_application():
     pass
 
