e88386da90d2227c8677538b9ce2182249588d52
Maxim Kirillov <maximkir181210@gmail.com>
2023-03-30 08:48:54 +0300

    Создал класс Passport

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+from typing import Dict
+
+
+# Задание 1.
+# Создайте класс Passport (паспорт), который будет содержать
+# паспортную информацию о гражданине страны. С помощью механизма
+# наследования, реализуйте класс ForeignPassport (загран.паспорт)
+# производный от Passport.
+
+class Passport:
+
+    def __init__(self, series: str, passport_id: str, full_name: Dict[str, str], pages: int):
+        pass
+
+
 def execute_application():
     pass
 
