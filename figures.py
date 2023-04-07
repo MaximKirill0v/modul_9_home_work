859d9d9d897c2435489cbd17ac845f51265f6274
Maxim Kirillov <maximkir181210@gmail.com>
2023-04-07 17:19:33 +0300

    Создал дочерний класс Square от класса Shape

--- figures.py
+++ figures.py
@@ -1,5 +1,3 @@
-
-
 class Shape:
     def __init__(self, x: int, y: int):
         self.__x = x
@@ -22,4 +20,20 @@ class Shape:
         self.__y = y
 
 
+class Square(Shape):
+    def __init__(self, x: int, y: int, side: float):
+        super().__init__(x, y)
+        self.__side = side
 
+    @property
+    def side(self):
+        return self.__side
+
+    @side.setter
+    def side(self, side):
+        self.__side = side
+
+    def info(self):
+        print(f"Класс фигуры: {self.__class__.__name__}\n"
+              f"Сторона квадрата равна: {self.__side}\n"
+              f"Координата верхнего левого угла равна: ({self.x}, {self.y})")
