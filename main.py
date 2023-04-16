f4b8405f75d509e71b1edd4011dee07809b940aa
Maxim Kirillov <maximkir181210@gmail.com>
2023-04-16 14:57:28 +0300

    Релиз ветки dev/home-9.12.1

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ from mixin_tyre import *
 # наследникам методы, которые выводят информацию об объекте из классов
 # «Колесо», «Двигатель», «Двери» и т.п.
 def execute_application():
-    gas_engine = Engine()
+    # Машина с бензиновым двигателем
     gas_car = CarGasEngine("Mazda", "Кроссовер", "Серый металлик", 2014, 95)
     gas_car.info()
     # Проверка статуса двигателя
+    gas_engine = Engine()
     gas_car.get_status_engine(gas_engine)
     # Проверка давления в колесе
     gas_car_tyre = TyrePressure()
@@ -26,10 +27,11 @@ def execute_application():
     gas_car.get_status_conditioner(conditioner)
     print()
 
-    diesel_engine = Engine()
+    # Машина с дизельным двигателем
     diesel_car = CarDieselEngine("Рено", "Универсал", "Белый", 2010, "ДТЛ")
     diesel_car.info()
     # Проверка статуса двигателя
+    diesel_engine = Engine()
     diesel_engine.state = True
     diesel_car.get_status_engine(diesel_engine)
     # Проверка давления в колесе
