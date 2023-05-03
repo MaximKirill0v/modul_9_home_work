5e661dd3a3682277e45504fbb0754d13ea3bff06
Maxim Kirillov <maximkir181210@gmail.com>
2023-05-04 00:02:57 +0300

    Добавил класс Client

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from abc import ABC, abstractmethod
 
 
+# Метод send_message должен отправлять сообщение в соответствии с типом отправки: по смс, по емейл и т.д.
+# Методам ваших классов не хватает параметров, чтобы выполнять свой функционал,
+# вместо этого в классы для чего-то были добавлены атрибуты почты и номера телефона.
+# Получается что для каждого клиента будь у него и отправка по номеру и по почте придется создать 2 экземпляра класса?
+# Не рационально! Продумать другую реализацию...
+
 # Задание 1.
 # Принцип открытости-закрытости закрепим на примере созданного в
 # задании 1 класса по отправке сообщений NotificationService.
@@ -30,6 +36,19 @@ from abc import ABC, abstractmethod
 # открытости-закрытости, так как мы расширяем нашу функциональность, а не
 # изменяем (модифицируем) наш класс.
 
+class Client:
+    def __init__(self, email: str, phone_number: str):
+        self.__email = email
+        self.__phone_number = phone_number
+
+    @property
+    def email(self):
+        return self.__email
+
+    @property
+    def phone_number(self):
+        return self.__phone_number
+
 
 class NotificationService(ABC):
     @abstractmethod
@@ -38,26 +57,27 @@ class NotificationService(ABC):
 
 
 class EmailNotification(NotificationService):
-    def __init__(self, email: str):
-        self.__email = email
+    def __init__(self, client: Client):
+        self.__email = client.email
 
     def send_message(self):
         print(f"Сообщение отправлено по электронной почте по адресу: '{self.__email}'")
 
 
 class MobileNotification(NotificationService):
-    def __init__(self, phone_number: str):
-        self.__phone_number = phone_number
+    def __init__(self, client: Client):
+        self.__phone_number = client.phone_number
 
     def send_message(self):
         print(f"Сообщение отправлено на номер: '{self.__phone_number}'")
 
 
 def execute_application():
-    email = EmailNotification("maximkir@gmail.com")
-    email.send_message()
+    client = Client("maximkir@gmail.com", "8-903-777-44-33")
 
-    phone_number = MobileNotification("8-903-777-44-33")
+    email = EmailNotification(client)
+    phone_number = MobileNotification(client)
+    email.send_message()
     phone_number.send_message()
 
 
