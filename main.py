631d273b240e86732f3d553304d00c1bbc866143
Maxim Kirillov <maximkir181210@gmail.com>
2023-06-06 23:13:53 +0300

    bug/fix-9.19.1

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from abc import ABC, abstractmethod
-
 
 # Задание 1.
 # Создайте класс «Число», которое хранит его значение и информацию о
@@ -15,8 +13,8 @@ class NotationValueError(Exception):
         self.__text = text
 
 
-class Number:
-    def __init__(self, notation: int, num: str):
+class Digit:
+    def __init__(self, notation: str, num: str):
         self.__notation = notation
         self.__num = num
 
@@ -28,99 +26,85 @@ class Number:
     def num(self):
         return self.__num
 
-    @num.setter
-    def num(self, value):
-        self.__num = value
-
     @staticmethod
-    def __is_notation(notation: int):
-        if isinstance(notation, int):
-            return notation in (2, 8, 10, 16)
+    def __is_notation(notation: str):
+        if isinstance(notation, str):
+            return notation in ('bin', 'oct', 'dec', 'hex')
         return False
 
     def __is_num(self, number: str):
-        if self.__notation == 2:
+        if self.__notation == 'bin':
             for num in number:
                 if num not in '01':
                     return False
             return True
-        elif self.__notation == 8:
+
+        elif self.__notation == 'oct':
             for num in number:
                 if num not in '01234567':
                     return False
             return True
-        elif self.__notation == 10:
+
+        elif self.__notation == 'dec':
             for num in number:
                 if num not in '0123456789':
                     return False
             return True
-        elif self.__notation == 16:
+
+        elif self.__notation == 'hex':
             for num in number:
-                if num not in '0123456789ABCDEF':
+                if num not in '0123456789abcdefABCDEF':
                     return False
             return True
 
     def __setattr__(self, key, value):
-        if key == '_Number__notation' and not self.__is_notation(value):
-            raise ValueError(f"Не корректная запись системы счисления: {self.__notation}")
-        if key == '_Number__num' and not self.__is_num(value):
-            raise ValueError(f"Не корректная запись числа: {value} в {self.__notation}-ой системе счисления.")
+        if key == '_Digit__notation' and not self.__is_notation(value):
+            raise ValueError(f"Не корректная запись системы счисления: {value}")
+        if key == '_Digit__num' and not self.__is_num(value):
+            raise ValueError(f"Не корректная запись числа: {value} в системе счисления {self.__notation}")
         super().__setattr__(key, value)
 
     def __str__(self):
         return f"Число: {self.__num}, система счисления: {self.__notation}"
 
 
-class NumberSystemsCalculator(ABC):
-    @staticmethod
-    @abstractmethod
-    def conversion_number(num: Number):
-        raise NotImplementedError
+class NumberSystemsCalculator:
+    NOTATION_DICT = {'bin': 2, 'oct': 8, 'dec': 10, 'hex': 16}
 
-
-class BinaryNumberSystem(NumberSystemsCalculator):
-    @staticmethod
-    def conversion_number(num: Number):
-        if num.notation != 2:
-            return f"Число в {num.notation}-ой СС {num.num} в двоичной СС равно: {bin(int(str(int(num.num, num.notation))))[2:]}"
+    def digit_to_bin(self, digit: Digit):
+        if digit.notation != 'bin':
+            return Digit('bin', bin(int(str(int(digit.num, self.NOTATION_DICT[digit.notation]))))[2:])
         raise NotationValueError(f"Ошибка конвертации. Перевод в ту же СС.")
 
-
-class OctalNumberSystem(NumberSystemsCalculator):
-    @staticmethod
-    def conversion_number(num: Number):
-        if num.notation != 8:
-            return f"Число в {num.notation}-ой СС {num.num} в восьмеричной СС равно: {oct(int(str(int(num.num, 16))))[2:]}"
+    def digit_to_oct(self, digit: Digit):
+        if digit.notation != 8:
+            return Digit('oct', oct(int(str(int(digit.num, self.NOTATION_DICT[digit.notation]))))[2:])
         raise NotationValueError(f"Ошибка конвертации. Перевод в ту же СС.")
 
-
-class DecimalNumberSystem(NumberSystemsCalculator):
-    @staticmethod
-    def conversion_number(num: Number):
-        if num.notation != 10:
-            return f"Число в {num.notation}-ой СС {num.num} в десятеричной СС равно: {int(num.num, num.notation)}"
+    def digit_to_dec(self, digit: Digit):
+        if digit.notation != 10:
+            return Digit('dec', str(int(digit.num, self.NOTATION_DICT[digit.notation])))
         raise NotationValueError(f"Ошибка конвертации. Перевод в ту же СС.")
 
-
-class HexadecimalNumberSystem(NumberSystemsCalculator):
-    @staticmethod
-    def conversion_number(num: Number):
-        if num.notation != 16:
-            return f"Число в {num.notation}-ой СС {num.num} в шестнадцатеричной СС равно: {hex(int(num.num))[2:]}"
+    def digit_to_hex(self, digit: Digit):
+        if digit.notation != 16:
+            return Digit('hex', hex(int(digit.num, self.NOTATION_DICT[digit.notation]))[2:])
         raise NotationValueError(f"Ошибка конвертации. Перевод в ту же СС.")
 
 
 def execute_application():
     try:
-        number_2 = Number(2, '101110')
-        number_8 = Number(8, '257')
-        number_10 = Number(10, '29')
-        number_16 = Number(16, '4F5')
+        number = Digit('dec', '1000')
         try:
-            print(BinaryNumberSystem.conversion_number(number_8))
-            print(OctalNumberSystem.conversion_number(number_2))
-            print(DecimalNumberSystem.conversion_number(number_16))
-            print(HexadecimalNumberSystem.conversion_number(number_10))
+            calculator = NumberSystemsCalculator()
+
+            print(calculator.digit_to_hex(number))
+
+            print(calculator.digit_to_oct(number))
+
+            print(calculator.digit_to_bin(number))
+
+            print(calculator.digit_to_dec(number))
 
         except NotationValueError as e:
             print(e)
